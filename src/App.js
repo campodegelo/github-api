fb8664a53b8d0385854d665f8a9ed9976ee313d7
eisfeld <gui.eisfeld@gmail.com>
2020-09-21 17:39:48 +0200

    Github Layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import SearchBox from './Components/SearchBox';
-
+import './css/style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Github API</h1>
+      <header className="header">
+        <h1 className="header__text-box">Github API</h1>
+        <img src="/github.png" alt="github" className="header__logo"/>
       </header>
 
       <SearchBox></SearchBox>
--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -5,7 +5,9 @@ const SearchBox = (props) => {
     const baseUrl = 'https://api.github.com';
 
     const [error, setError] = useState(false);
+    const [option, setOption] = useState();
     const [results, setResults] = useState();
+    const [user, setUser] = useState();
     const [values, setValues] = useState();
     
 
@@ -19,13 +21,21 @@ const SearchBox = (props) => {
     const handleSearch = (username, endpoint) => {
         console.log("values = ", username);
         const request = '/users/' + username + endpoint;
+
         (async () => {
 
             try {
                 const {data} = await axios.get(baseUrl + request);
                 console.log('data from axios request to github API = ', data);
                 setError(false);
-                setResults(data);
+
+                if (endpoint === '') {
+                    setUser(data);
+                } else {
+                    setOption(endpoint);
+                    setResults(data);
+                }
+
             } catch(err) {
                 console.log('error fetching information from github api: ', err);
                 setError(true);
@@ -40,6 +50,10 @@ const SearchBox = (props) => {
         // checks if the username is part of the pathname
         console.log('document = ', document.location.pathname.split('/')[1]);
         if (document.location.pathname !== '/') {
+            setValues({
+                ...values,
+                'username': document.location.pathname.split('/')[1]
+            });
             handleSearch(document.location.pathname.split('/')[1], '');
         }
 
@@ -52,13 +66,11 @@ const SearchBox = (props) => {
                 <p className="error">ERROR</p>
             )}
 
-            {/* if there is no username in the url, show the form with the input field */}
-            {!results && (
+            <div className="side-menu">
                 <div className="form">
                     <div className="form__group">
                         <input
-                            type="text"
-                            name="username"
+                            type="text"                            name="username"
                             placeholder="Username to be searched"
                             className="form__input"
                             autoComplete="off"
@@ -73,42 +85,88 @@ const SearchBox = (props) => {
                     </div>
 
                     <button
-                        className="btn btn--animated"
+                        className="btn btn--animated btn--primary"
                         onClick={() => handleSearch(values.username, '')}
                     >search
                     </button>
                 </div>
-            )}
+            </div>
 
-            {/* if there is a username in the url, show the results from the API inquiry */}
-            {results && (
-                <div className="result">
-                    <h2 className="result__name">
-                        {results.name}
-                    </h2>
+            <div className="content">
 
-                    <h3 className="result__name">
-                        {results.login}
-                    </h3>
+                {/* if there is a username in the url, show the user information from the API inquiry */}
+                {user && (
+                    <div className="user">
+                        <h2 className="user__name">
+                            {user.name}
+                        </h2>
 
-                    <img src={results.avatar_url} alt={results.login} className="results__image"/>
+                        <h3 className="user__name">
+                            {user.login}
+                        </h3>
+
+                        <img src={user.avatar_url} alt={user.login} className="user__image"/>
 
 
-                    <button 
-                        className="btn btn--animated"
-                        onClick={() => handleSearch(values.username, '/repos')}
-                    >repos
-                    </button>
-                    
-                    <button 
-                        className="btn btn--animated"
-                        onClick={() => handleSearch(values.username, '/starred')}
-                    >starred
-                    </button>
+                        <button 
+                            className="btn btn--animated"
+                            onClick={() => handleSearch(values.username, '/repos')}
+                        >repos
+                        </button>
+                        
+                        <button 
+                            className="btn btn--animated"
+                            onClick={() => handleSearch(values.username, '/starred')}
+                        >starred
+                        </button>
 
-                </div>
+                    </div>
 
-            )}
+                )}
+
+                {/* if repos or starred requests were made, show results */}
+                {results && (
+                    <div className="results">
+                        <h3 className="results__title">results</h3>
+
+                        {option && option==='/repos' && (
+                            <div className="results__container">
+                                {results.map(r => (
+                                    <div className="results__item" key={r.id}>
+                                        <a 
+                                            href={r.html_url}            target="__blank" className="results__link"
+                                        >{r.name}
+                                        </a>
+                                        
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+
+                        {option && option==='/starred' && (
+                            <div className="results__container">
+                                {results.map(r => (
+                                    <div className="results__item" key={r.id}>
+                                        <p className="results__repo">
+                                            {r.full_name}
+                                        </p>
+
+                                        <div className="results__moreInfo">
+                                            <p className="p results__moreInfo--login">{r.owner.login}</p>
+                                            <img src={r.owner.avatar_url} alt={r.login} className="results__moreInfo--avatar"/>
+
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+
+
+                    </div>
+                )}
+            </div>
+
+
 
         </div>
         
